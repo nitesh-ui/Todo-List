8943e5b18603a1e27e0c5b3b30db68944f99d60e
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-09-12 04:41:29 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import './App.css';
+import AddTodo from './components/AddTodo';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Todos from './components/Todos';
@@ -6,9 +8,12 @@ import Todos from './components/Todos';
 function App() {
   const onDelete = (todo) => {
     console.log("I'm on delete", todo);
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
   }
 
-  let todos = [
+  const [todos, setTodos] = useState([
     {
       sno: 1,
       title: "Go to the market",
@@ -24,11 +29,32 @@ function App() {
       title: "Go to the kashi",
       desc: "You need to go to the market to get this job done3"
     }
-  ]
+  ]);
+
+  const addTodo = (title, desc) => {
+    console.log("Adding new todo item-->", title, desc);
+
+    let sno;
+    if(todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+
+  }
 
   return (
     <div>
       <Header title="Todo List" />
+      <AddTodo addTodo={addTodo}/>
       <Todos todos={todos} onDelete={onDelete}/>
       <Footer/>
     </div>
